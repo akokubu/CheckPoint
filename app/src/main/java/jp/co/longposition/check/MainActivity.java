c100a75ab1fd1fc6a1af521d71a45db0e1838956
akokubu <akokubu@longposition.co.jp>
2014-01-05 21:34:46 +0900

    Google Map使えるように設定

--- app/src/main/java/jp/co/longposition/check/MainActivity.java
+++ app/src/main/java/jp/co/longposition/check/MainActivity.java
@@ -1,15 +1,19 @@
 package jp.co.longposition.check;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -23,8 +27,9 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-    }
 
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,6 +56,8 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        private GoogleMap googleMap;
+
         public PlaceholderFragment() {
         }
 
@@ -58,7 +65,36 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map);
+            try {
+                googleMap = mapFragment.getMap();
+                if (savedInstanceState == null) {
+                    mapFragment.setRetainInstance(true);
+
+                    mapinit();
+                }
+            } catch (Exception e) {
+            }
+
             return rootView;
+        }
+
+        private void mapinit() {
+
+            // 地図タイプ設定
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+            // 現在位置ボタンの表示
+            googleMap.setMyLocationEnabled(true);
+
+            // 東京駅の位置、ズーム設定
+            CameraPosition camerapos = new CameraPosition.Builder()
+                .target(new LatLng(35.681382, 139.766084)).zoom(15.5f).build();
+
+            // 地図の中心を変更する
+            googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(camerapos));
+
         }
     }
 
